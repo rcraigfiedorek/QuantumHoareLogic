dd8a422629231a26152990e813ee34973867ac86
rcraigfiedorek <rcraigfiedorek@Craigs-MBP-7.lan1>
2020-09-10 18:10:50 -0400

    renamed gitignore file so it actually works lol

--- .coqdeps.d
+++
@@ -1,6 +0,0 @@
-theories/Complex.vo theories/Complex.glob theories/Complex.v.beautified: theories/Complex.v
-theories/Complex.vio: theories/Complex.v
-theories/Matrix.vo theories/Matrix.glob theories/Matrix.v.beautified: theories/Matrix.v theories/Complex.vo
-theories/Matrix.vio: theories/Matrix.v theories/Complex.vio
-theories/QNL.vo theories/QNL.glob theories/QNL.v.beautified: theories/QNL.v theories/Matrix.vo
-theories/QNL.vio: theories/QNL.v theories/Matrix.vio
